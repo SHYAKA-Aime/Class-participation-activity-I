aecf1c1c6dfe8c4c56b847500661e32830d7b823
Aime Brues <72033556+SHYAKA-Aime@users.noreply.github.com>
2025-02-26 11:43:28 +0200

    Create userinput.py

---
+++ userinput.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+num1 = int(num1)
+num2 = int(num2)
+
+result = num1 + num2
+
+print("The result of adding", num1, "and", num2, "is:", result)
